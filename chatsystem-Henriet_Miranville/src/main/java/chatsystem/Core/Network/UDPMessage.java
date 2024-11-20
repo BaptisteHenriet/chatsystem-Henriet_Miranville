a3a57072d46b9c70c8e7b68f6d49e50bdec09073
BaptisteHenriet <119511364+BaptisteHenriet@users.noreply.github.com>
2024-11-20 22:52:46 +0100

    Delete chatsystem-Henriet_Miranville directory

--- chatsystem-Henriet_Miranville/src/main/java/chatsystem/Core/Network/UDPMessage.java
+++
@@ -1,7 +0,0 @@
-package chatsystem.Core.Network;
-
-import java.net.InetAddress;
-
-public record UDPMessage(String content, InetAddress origin) {
-
-}
--- chatsystem-Henriet_Miranville/src/main/java/chatsystem/Interface/Contact.java
+++
@@ -1,4 +0,0 @@
-package chatsystem.Interface;
-
-public record Contact(String username) {
-}
